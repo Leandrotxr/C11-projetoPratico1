e615b1483e3d8d7130a8ae86547d62304167e7d0
Pedro Paulo Junqueira <pepejunqueira@hotmail.com>
2025-10-15 16:10:52 -0300

    atualizando as bibliotecas

--- br/inatel/cdg/project/main.py
+++ br/inatel/cdg/project/main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-dataset = pd.read_csv('../data/student_lifestyle_dataset.csv', delimiter=';')
+dataset = pd.read_csv('../data/student_lifestyle_dataset.csv', delimiter=',')
 
-print("✅ Dataset carregado com sucesso!\n")
+print("Dataset carregado com sucesso!\n")
 
 dataset = dataset.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
 dataset = dataset.replace(r'^\s*$', np.nan, regex=True)
-
 
 print(dataset)
