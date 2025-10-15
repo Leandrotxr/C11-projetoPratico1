bfd8dd024f90b908830365e3c884621397c20197
Leandro Teixeira <leandro7teixeita@gmail.com>
2025-10-14 23:48:02 -0300

    docs: alterando o dataset

--- br/inatel/cdg/project/main.py
+++ br/inatel/cdg/project/main.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('../data/data.csv', delimiter=';')
+dataset = pd.read_csv('../data/student_lifestyle_dataset.csv', delimiter=';')
 
 print("✅ Dataset carregado com sucesso!\n")
-print(df.head())
 
+dataset = dataset.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
+dataset = dataset.replace(r'^\s*$', np.nan, regex=True)
+
+
+print(dataset)
